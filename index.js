b5f281957b504bbd05273ed9fd5d15e328242dfb
Austin Sullivan <austinmclastro@gmail.com>
2018-03-07 11:59:36 -0700

    CSS now shows up when connecting to the chat
    Also fleshed out the chat and user windows a bit

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var path = (require('path'));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.get(__dirname + '/mainStyle.css');
 });
 
 io.on('connection', function(socket){
+    console.log('\nA user connected');
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+    socket.on('disconnect', function(){
+       console.log('\nA user disconnected'); 
+    });
 });
 
 http.listen(port, function(){
